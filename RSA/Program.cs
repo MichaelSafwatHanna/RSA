1d630e7fd725546c0890e2ea24aa89eca61152d8
MichelineMedhat <michelinemedhat@gmail.com>
2019-12-06 03:48:47 +0200

    Implement Subtract Method

--- RSA/Program.cs
+++ RSA/Program.cs
@@ -8,12 +8,12 @@ namespace RSA
         private static void Main(string[] args)
         {
             var big = new BigInteger("1000000000000000001");
-            var result = big.Add(new BigInteger("1"));
+            var result = big.Subtract(new BigInteger("1"));
             
             Console.WriteLine($"Result: {result}");
             Console.WriteLine($"Size: {result.Size}");
             Console.WriteLine($"Clusters: {result.ClustersLength}");
-            Console.WriteLine($"As Expected: {result.ToString() == "1000000000000000002"}");
+            Console.WriteLine($"As Expected: {result.ToString() == "1000000000000000000"}");
         }
     }
 }
