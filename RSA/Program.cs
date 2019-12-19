d8bb75d9a2e82c1a9d9994040b2a3b42b5cfba79
MichelineMedhat <michelinemedhat@gmail.com>
2019-12-19 05:52:23 +0200

    Implement Encoder Class

--- RSA/Program.cs
+++ RSA/Program.cs
@@ -7,19 +7,19 @@ namespace Crypto.RSA
     {
         private static void Main(string[] args)
         {
-            var operand1 = new BigInteger("3");
-            var operand2 = new BigInteger("7");
-            var actual = operand1.ModOfPower(operand2, new BigInteger("5"));
-            var expect = new BigInteger("2");
+            var input = RSA.Encoder.GetBytes("Zero Based");
+            var n = new BigInteger("47594980475625417724408267823112764463863576918685226363032787239910118740004860624166859668486833021538759738968887527");
+            var e = new BigInteger("17");
+            var d = new BigInteger("22397637870882549517368596622641300924171095020557753582603446902846197377658196974714575577237681892436409853219169457");
 
-            Console.WriteLine();
-            Console.WriteLine($"    [OPRND1]     {operand1} | Length: {operand1.Length} | Clusters: {operand1.ClustersLength}");
-            Console.WriteLine($"    [OPRND2]     {operand2} | Length: {operand2.Length} | Clusters: {operand2.ClustersLength}");
-            Console.WriteLine($"    [Actual]     {actual} | Length: {actual.Length} | Clusters: {actual.ClustersLength}");
-            Console.WriteLine($"    [Expect]     {expect} | Length: {expect.Length} | Clusters: {expect.ClustersLength}");
-            Console.WriteLine($"    [Status]     {actual.Equals(expect)}");
-            Console.WriteLine();
+            BigInteger enc = RSA.Encrypt(n, e, new BigInteger(input));
+            BigInteger dec = RSA.Decrypt(n, d, enc);
 
+
+            string output = RSA.Encoder.GetString(dec.ToString());
+
+            Console.WriteLine("Encrypted : " + enc);
+            Console.WriteLine("You Entered : " + output);
 
         }
     }
--- RSA/RSA.cs
+++ RSA/RSA.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using Type.BigInteger;
+using static System.Int32;
 
 namespace Crypto.RSA
 {
     public static class RSA
     {
+        
         public static BigInteger Encrypt(BigInteger n, BigInteger e, BigInteger m)
         {
             return m.ModOfPower(e, n);
@@ -105,6 +107,52 @@ namespace Crypto.RSA
                 var e = GeneratePublicKey(phi, n);
                 var d = GeneratePrivateKey(e, phi);
                 return (n, e, d);
+            }
+
+        }
+
+        public class Encoder
+        {
+            private const int MaxAsciiLength = 3;
+
+            public static string GetBytes(string str)
+            {
+                var length = str.Length * MaxAsciiLength;
+                var result = new char[length];
+
+                for (var i = 0; i < length; i += MaxAsciiLength)
+                {
+                    int ascii = str[i / MaxAsciiLength];
+                    var asciiStr = ascii.ToString();
+                    var offset = MaxAsciiLength - asciiStr.Length;
+
+                    for (var j = 0; j < MaxAsciiLength; j++)
+                    {
+                        result[i + j] = j < offset ? '0' : asciiStr[j - offset];
+                    }
+                }
+
+                return new string(result);
+            }
+
+            public static string GetString(string bytes)
+            {
+                var length = (int)Math.Ceiling(bytes.Length / (double)MaxAsciiLength);
+                var result = new char[length];
+
+                for (var i = bytes.Length - MaxAsciiLength; i >= 0; i -= MaxAsciiLength)
+                {
+                    result[length - 1] = (char)Convert.ToInt32(bytes.Substring(i, MaxAsciiLength));
+                    length--;
+                }
+
+                var remainder = bytes.Length % MaxAsciiLength;
+                if (remainder != 0)
+                {
+                    result[0] = (char)Convert.ToInt32(bytes.Substring(0, remainder));
+                }
+
+                return new string(result);
             }
 
         }
